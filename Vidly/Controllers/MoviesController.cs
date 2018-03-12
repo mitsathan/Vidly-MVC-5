441bbf34fa3bcafcb361f7d3e94e801f5d2913e4
mitsathan <dm.athanasopoulos@gmail.com>
2018-03-12 21:10:45 +0200

    Add project files.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() {Name = "Shrek!"};
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "customer 1"},
+                new Customer {Name = "customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            //ViewData["Movie"] = movie;
+
+            return View(viewModel);
+            //return Content("hello world!");
+            //return HttpNotFound();
+            //return  new EmptyResult();
+            //return RedirectToAction("index", "Home",new {page=1,sortby="name"});
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        //movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "name";
+
+            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        [Route("movies/released/{year}/{month:range(1,12)}")]
+        public ActionResult ByReleaseDate(int year,int month)
+        {
+            return Content(year+"/"+month);
+        }
+    }
+}
